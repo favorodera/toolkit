fb74df12e5d6a76cdb7d65e7b697f972e005ef0d
Favour  Emeka <favorodera@gmail.com>
2025-12-03 16:39:13 +0100

    feat(header): add header component

---
+++ app/components/app/header.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <header
+    class="
+      sticky top-0 z-50 flex h-(--header-height) w-full items-center gap-4
+      bg-background px-6
+    "
+  >
+
+    <UiButton
+      variant="ghost"
+      class="text-lg font-bold"
+    >
+      <Icon
+        name="lucide:tool-case"
+        class="size-5"
+      />
+      TOOLKIT
+    </UiButton>
+
+  </header>
+</template>
