f7d0fd4606d2d0540d744a47b86f97d1e8d071f7
Favour  Emeka <favorodera@gmail.com>
2025-12-03 16:39:30 +0100

    feat(landing): create tools grid component

---
+++ app/components/landing/tools-grid.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <section class="w-full">
+
+    <div
+      class="
+        container mx-auto grid grid-cols-1 items-center gap-2 py-8
+        sm:grid-cols-2
+        lg:grid-cols-3
+      "
+    >
+
+      <UiItem
+        v-for="tool, index in toolsRegistry"
+        :key="index"
+        variant="outline"
+        as-child
+      >
+
+        <NuxtLink :to="tool.path">
+
+          <UiItemMedia>
+            <Icon
+              :name="tool.icon"
+              class="size-12"
+            />
+          </UiItemMedia>
+
+          <UiItemContent>
+            <UiItemTitle>{{ tool.name }}</UiItemTitle>
+            <UiItemDescription>{{ tool.description }}</UiItemDescription>
+          </UiItemContent>
+
+        </NuxtLink>
+
+      </UiItem>
+
+    </div>
+
+  </section>
+</template>
