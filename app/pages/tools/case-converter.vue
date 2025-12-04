2252abd5dda2f5188e3de7a3754c515b68ffec6a
Favour  Emeka <favorodera@gmail.com>
2025-12-04 09:36:52 +0100

    feat(case-converter): add case converter tool

---
+++ app/pages/tools/case-converter.vue
@@ -0,0 +1,109 @@
+<script lang="ts" setup>
+import { useChangeCase, type ChangeCaseType } from '@vueuse/integrations/useChangeCase'
+
+definePageMeta({
+  layout: 'tool',
+  toolName: 'Case Converter',
+})
+
+const mainCaseTypes = ref<ChangeCaseType[]>([
+  'noCase',
+  'camelCase',
+  'pascalCase',
+  'pascalSnakeCase',
+  'capitalCase',
+  'constantCase',
+  'dotCase',
+  'kebabCase',
+  'pathCase',
+  'sentenceCase',
+  'snakeCase',
+  'trainCase',
+])
+
+const input = ref('')
+const copiedKey = ref<string | number | null>(null)
+
+const output = computed(() => {
+  const results = new Map<string, string>()
+
+  mainCaseTypes.value.forEach((type) => {
+    results.set(useChangeCase(type, type).value, useChangeCase(input.value.trim() || 'Enter text to convert', type).value)
+  })
+
+  return Object.fromEntries(results.entries())
+})
+
+const copy = async (key: string | number, text: string) => {
+  const { copy } = useClipboard({ legacy: true })
+
+  await copy(text)
+
+  copiedKey.value = key
+
+  setTimeout(() => {
+    copiedKey.value = null
+  }, 2000)
+}
+</script>
+
+<template>
+  <section>
+    <div
+      class="
+        @container relative grid gap-8
+        lg:grid-cols-2
+      "
+    >
+      <UiTextarea
+        v-model="input"
+        spellcheck
+        autocorrect
+        class="h-[calc(100dvh-30rem)]"
+        placeholder="Type or paste text here ..."
+      />
+
+      <div
+        class="
+          max-h-[calc(100dvh-16rem)] self-start overflow-y-auto rounded-md
+          border px-4 pb-4 scrollbar-thin
+        "
+      >
+        <p class="sticky top-0 bg-background py-4 text-lg font-semibold">
+          Select Preferred Case
+        </p>
+
+        <UiItemGroup class="gap-4">
+          <UiItem
+            v-for="result, key in output"
+            :key
+            variant="outline"
+            class="flex-nowrap"
+          >
+            <UiItemContent>
+              <UiItemTitle>{{ key }}</UiItemTitle>
+              <UiItemDescription class="line-clamp-1 break-all">
+                {{ result }}
+              </UiItemDescription>
+            </UiItemContent>
+
+            <UiItemActions>
+              <UiButton
+                size="icon-sm"
+                variant="outline"
+                @click="copy(key, result)"
+              >
+                <Icon :name="copiedKey === key ? 'lucide:copy-check' : 'lucide:copy'" />
+              </UiButton>
+            </UiItemActions>
+
+          </UiItem>
+        </UiItemGroup>
+
+      </div>
+
+    </div>
+
+  </section>
+
+</template>
