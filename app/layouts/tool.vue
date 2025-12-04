9dee0c50661c6b39d436bd92fe49a6ccfde42b60
Favour  Emeka <favorodera@gmail.com>
2025-12-04 07:07:23 +0100

    feat(tool): create tool layout

---
+++ app/layouts/tool.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+const route = useRoute()
+
+const toolName = computed(() => route.meta.toolName ?? '')
+</script>
+
+<template>
+  
+  <div
+    class="
+      relative flex min-h-svh flex-col gap-4 px-2
+      sm:px-4
+    "
+  >
+    <AppHeader />
+
+    <main class="container mx-auto flex flex-1 flex-col gap-6">
+
+      <div class="mr-auto flex items-center gap-4">
+
+        <UiButton
+          as-child
+          variant="ghost"
+          size="icon"
+        >
+          <NuxtLink to="/tools">
+            <Icon
+              name="lucide:chevron-left"
+              class="size-5"
+            />
+          </NuxtLink>
+        </UiButton>
+
+        <h1 class="text-lg font-semibold">
+          {{ toolName }}
+        </h1>
+
+      </div>
+
+      <slot />
+    </main>
+
+  </div>
+
+</template>
