b6b0535057f3397978ec890d9d3f71575e7e581b
Favour  Emeka <favorodera@gmail.com>
2025-12-09 19:52:50 +0100

    feat(qr-code-generator): refactor URL input with validation and structured form layout

--- app/components/app/tools/qr-code-generator/url.vue
+++ app/components/app/tools/qr-code-generator/url.vue
@@ -1,20 +1,55 @@
 <script lang="ts" setup>
+import { Field as VeeField, useForm } from 'vee-validate'
+import { toTypedSchema } from '@vee-validate/zod'
+import { z } from 'zod'
+
+const schema = toTypedSchema(z.object({
+  url: z.url('Invalid URL'),
+}))
+
+const { errors: formErrors } = useForm({
+  validationSchema: schema,
+})
+
+const qrCodeData = useState('qr-code-data')
+
 const inputModel = ref('')
 
-watch(inputModel, (value) => {
-  useState('qr-code-data').value = value
+watch([inputModel, formErrors], ([value, errors]) => {
+  qrCodeData.value = Object.keys(errors).length ? ' ' : value
 })
 </script>
 
 <template>
+  <form id="qr-code-url-form">
 
-  <UiInput
-    id="qr-code-generator-url"
-    v-model="inputModel"
-    autocomplete="url"
-    placeholder="Enter or paste url"
-    type="url"
-  />
+    <VeeField
+      v-slot="{ field, errors }"
+      v-model="inputModel"
+      name="url"
+      validate-on-input
+    >
 
+      <UiField
+        :data-invalid="!!errors.length"
+        :name="field.name"
+      >
+        <UiInput
+          autocomplete="url"
+          placeholder="Enter or paste url"
+          type="url"
+          v-bind="field"
+          :aria-invalid="!!errors.length"
+        />
+
+        <UiFieldError
+          v-if="errors.length"
+          :errors="errors"
+        />
+      </UiField>
+
+    </VeeField>
+
+  </form>
 </template>
 
