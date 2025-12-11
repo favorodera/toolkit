1dbc821c9838af0da2e9a70d9255d691fe7dbdef
Favour  Emeka <favorodera@gmail.com>
2025-12-11 18:08:02 +0100

    feat(qr-code-generator): add tel input component

---
+++ app/components/app/tools/qr-code-generator/tel.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const { QRCodeTelData } = storeToRefs(useQRCodeStore())
+</script>
+  
+<template>
+  <form id="qr-code-tel-form">
+  
+    <UiField
+      orientation="responsive"
+      name="tel"
+    >
+  
+      <UiFieldContent>
+  
+        <UiInput
+          id="tel"
+          v-model="QRCodeTelData"
+          autocomplete="tel"
+          placeholder="Enter or paste phone number"
+          type="tel"
+        />
+        
+      </UiFieldContent>
+            
+    </UiField>
+  
+  </form>
+</template>
+  
+  
