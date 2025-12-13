f38f42fd47bcc42a8cdb0e7dcb5ae4da8c9ebfd6
Favour  Emeka <favorodera@gmail.com>
2025-12-13 19:31:47 +0100

    feat(tools): Add UUID Generator page

---
+++ app/pages/tools/uuid-generator.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'tool',
+  toolName: 'UUID Generator',
+})
+
+useSeoMeta({ ...toolsSeo('UUID Generator') })
+</script>
+
+<template>
+  <section class="relative" />
+</template>
